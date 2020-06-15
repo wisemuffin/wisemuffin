06fbd03808e96b0f5ffe351fdc3351ba45537919
David Griffiths <davidgg777@hotmail.com>
2020-06-16 09:56:04 +1000

    added times to vis gallery

--- src/components/VisList/index.tsx
+++ src/components/VisList/index.tsx
@@ -5,12 +5,15 @@ import CardHeader from "@material-ui/core/CardHeader";
 import CardActions from "@material-ui/core/CardActions";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import AccessTime from "@material-ui/icons/AccessTime";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import { IAction, IVis, IVisListProps } from "../../interfaces";
 import IconButton from "@material-ui/core/IconButton";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import distanceFromWordsToNow from "date-fns/distance_in_words_to_now";
 
 const VisList: React.FC<IVisListProps> = ({ store, visualisations }) => {
   const { state, dispatch } = store;
@@ -40,8 +43,31 @@ const VisList: React.FC<IVisListProps> = ({ store, visualisations }) => {
               <Card>
                 <CardActionArea href={vis.link}>
                   <ChooseMedia />
-                  <CardContent>{vis.summary}</CardContent>
                   <CardHeader title={vis.name} />
+
+                  <CardContent>
+                    <Typography
+                      variant="subtitle1"
+                      color="inherit"
+                      gutterBottom
+                    >
+                      {vis.summary}
+                    </Typography>
+                    <Grid container spacing={2}>
+                      <Grid item>
+                        <AccessTime />
+                      </Grid>
+                      <Grid item>
+                        <Typography
+                          variant="subtitle2"
+                          color="inherit"
+                          gutterBottom
+                        >
+                          {distanceFromWordsToNow(vis.createdDate) + " ago"}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </CardContent>
                 </CardActionArea>
                 {/* <CardActions>
                   <IconButton
